bb9601e1bf9a4e3fd3390d282b43d2330926f01b
hashi <bluepatissere0310@iclud.com>
2022-10-11 09:27:43 +0900

    [change]
    クエリを修正
    
    担当者
    橋本 純一

--- GROUP_BY/group_by.sql
+++ GROUP_BY/group_by.sql
@@ -3,3 +3,29 @@
 SELECT class_score, COUNT(student_name)
   FROM ClassD
 GROUP BY class_score;
+
+/* 生徒のデータを追加 */
+START TRANSACTION;
+
+INSERT INTO
+  ClassD
+VALUES
+  ('0009', '佐藤', 52),
+  ('0010', '松下', 94),
+  ('0011', '平田', 98),
+  ('0012', '須藤', 78);
+
+COMMIT;
+
+SELECT * FROM ClassD;
+
+/*優秀な生徒の点数を95点に変更して合わせる*/
+UPDATE ClassD
+  SET class_score = 95
+WHERE student_name = '平田';
+
+UPDATE ClassD
+  SET class_score = 95
+WHERE student_name = '松下';
+
+SELECT * FROM ClassD;
--- HAVING/having.sql
+++ HAVING/having.sql
@@ -1,7 +1,7 @@
 /* 点数によって生徒をグループ分けする */
 /* 38点の生徒は3名いました! */
 /* 38点の行だけデータを表示する*/
-SELECT class_score, COUNT(student_name)
+SELECT COUNT(class_score)
   FROM ClassD
 GROUP BY class_score
 HAVING class_score = 38;
